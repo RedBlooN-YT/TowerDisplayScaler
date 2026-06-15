f30c0f587c3c6acc32c2c0911bc92c5f0aa1512b
RedBlooN-YT <monkeyfiesta2@gmail.com>
2026-06-14 19:13:16 -0700

    Add files via upload

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -4,10 +4,10 @@ public static class ModHelperData
 {
     public const string WorksOnVersion = "55.1";
     public const string Version = "2.0.0";
-    public const string Name = "TowerScaler";
+    public const string Name = "TowerDisplayScaler";
 
     public const string Description = "All towers now have buttons to change their size";
 
     public const string RepoOwner = "RedBlooN-YT";
-    public const string RepoName = "TowerScaler";
+    public const string RepoName = "TowerDisplayScaler";
 }
