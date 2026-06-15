863413e9f0f77d1856e4d4804ce4f98f9eba1b99
RedBlooN-YT <monkeyfiesta2@gmail.com>
2026-06-14 19:16:07 -0700

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "2.0.0";
     public const string Name = "TowerDisplayScaler";
 
-    public const string Description = "All towers now have buttons to change their size";
+    public const string Description = "All towers now have buttons to change their display scale";
 
     public const string RepoOwner = "RedBlooN-YT";
     public const string RepoName = "TowerDisplayScaler";
